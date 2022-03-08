3c9c5b982b232007b6619e4efc5e8fba78067347
teresacabell <teresacabll@gmail.com>
2022-03-08 16:29:45 -0600

    fixed dependencies in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const router = require('express').Router();
 
 // initialize express
 const PORT = process.env.PORT || 3001;
@@ -13,9 +14,10 @@ app.use(express.json());
 // maybe this should be __dirname?
 app.use(express.static('public'));
 
-// routes file
-require('./routes/routes')(app);
+
 // listener
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
+module.exports = router;
