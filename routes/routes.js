61b72451b2925f1f3a4df9748b82a2acd85fb4d8
teresacabell <teresacabll@gmail.com>
2022-03-08 16:25:37 -0600

    added delete and get by id

--- routes/routes.js
+++ routes/routes.js
@@ -7,10 +7,12 @@ router.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+// view index.html
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// view notes.html
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
@@ -22,6 +24,18 @@ router.post('/api/notes', (req, res) => {
     notes.push(newNote);
     updateDB();
     return console.log('New note created' + newNote.title);
+});
+
+// gets note by id
+router.get('/api/notes/:id', (req, res) => {
+    res.json(notes[req.params.id]);
+});
+
+// DELETE route for note by id
+router.delete('/api/notes/:id', (req, res) => {
+    notes.splice(req.params.id, 1);
+    updateDB();
+    console.log('Deleted ' + req.params.id + ' note');
 });
 
 // update JSON 
