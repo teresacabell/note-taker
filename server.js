b3c78a5baf2d8ab87d0279833b8aec0eac0da0b0
teresacabell <teresacabll@gmail.com>
2022-04-13 17:20:11 -0500

    transferred to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const database = require('./db/db.json')
-
+const router = require('express').Router();
 
 // initialize express
 const PORT = process.env.PORT || 3001;
@@ -16,6 +16,22 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// notes variable
+fs.readFile('db/db.json', (err, data) => {
+    if (err) throw err;
+
+    // otherwise create notes variable
+    var notes = JSON.parse(data);
+})
+
+// update JSON 
+function updateDB() {
+    fs.writeFile('db/db.json', JSON.stringify(notes,'\t'), err => {
+    if (err) throw err;
+    return true;
+    });
+}
+
 // html for index
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'))
@@ -25,6 +41,36 @@ app.get('/', function (req, res) {
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
+
+// displays index.html with anything after /api/notes/
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+// GET all notes
+app.get('/api/notes', (req, res) => {
+    res.json(database);
+})
+
+// gets note by id
+app.get('/api/notes/:id', (req, res) => {
+    res.json(notes[req.params.id]);
+});
+
+// post new note
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+    notes.push(newNote);
+    updateDB();
+    return console.log('New note created' + newNote.title);
+})
+
+// DELETE route for note by id
+app.delete('/api/notes/:id', (req, res) => {
+    notes.splice(req.params.id, 1);
+    updateDB();
+    console.log('Deleted ' + req.params.id + ' note');
+});
 
 
 // listener
