2e8c7ea678c8f902e3653e09bc63a2c6f66fb00e
teresacabell <teresacabll@gmail.com>
2022-04-13 18:26:04 -0500

    refactored get and post so they are together

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const database = require('./db/db.json')
-const router = require('express').Router();
+const database = require('./db/db.json');
+const res = require('express/lib/response');
 
 // initialize express
 const PORT = process.env.PORT || 3001;
@@ -24,13 +24,13 @@ fs.readFile('db/db.json', (err, data) => {
     var notes = JSON.parse(data);
 })
 
-// update JSON 
-function updateDB() {
-    fs.writeFile('db/db.json', JSON.stringify(notes,'\t'), err => {
-    if (err) throw err;
-    return true;
-    });
-}
+// // update JSON 
+// function updateDB() {
+//     fs.writeFile('db/db.json', JSON.stringify(database,'\t'), err => {
+//     if (err) throw err;
+//     return true;
+//     });
+// }
 
 // html for index
 app.get('/', function (req, res) {
@@ -42,32 +42,60 @@ app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
 
-// displays index.html with anything after /api/notes/
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+// GET POST DELETE
 
-// GET all notes
-app.get('/api/notes', (req, res) => {
-    res.json(database);
-})
 
-// gets note by id
-app.get('/api/notes/:id', (req, res) => {
-    res.json(notes[req.params.id]);
-});
+// GET and post notes can be combined 
+app.route('/api/notes')
+    
+// get notes list
+    .get(function (req, res) {
+        res.json(database);
+    })
 
-// post new note
-app.post('/api/notes', (req, res) => {
-    let newNote = req.body;
-    notes.push(newNote);
-    updateDB();
-    return console.log('New note created' + newNote.title);
-})
+// post a new note 
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, '/db/db.json');
+        let newNote = req.body;
+
+    let highId = 99;
+    newNote.id = highId + 1;
+    database.push(newNote)
+
+    fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Note successfully saved!");
+    });
+
+    res.json(newNote);
+    }); 
+
+
+
+
+// app.get('/api/notes', (req, res) => {
+//     res.json(database);
+// })
+
+// // gets note by id
+// app.get('/api/notes/:id', (req, res) => {
+//     res.json(database[req.params.id]);
+// });
+
+// // post new note
+// app.post('/api/notes', (req, res) => {
+//     let newNote = req.body;
+//     database.push(newNote);
+//     updateDB();
+//     return console.log('New note created' + newNote.title);
+// })
 
 // DELETE route for note by id
 app.delete('/api/notes/:id', (req, res) => {
-    notes.splice(req.params.id, 1);
+    database.splice(req.params.id, 1);
     updateDB();
     console.log('Deleted ' + req.params.id + ' note');
 });
